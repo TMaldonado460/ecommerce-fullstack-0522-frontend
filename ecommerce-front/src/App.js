0ebd507796426866875d554d92b423d13790be63
Guido Michel Oudin <guidooudin1@gmail.com>
2022-05-06 17:35:30 -0300

    nav bar component design done

--- ecommerce-front/src/App.js
+++ ecommerce-front/src/App.js
@@ -1,10 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from 'react-bootstrap/Navbar';
+
 
 import './App.css';
+import NavBarComp from './components/NavBarComp';
 
 function App() {
   return (
     <div className="App">
-      
+      <NavBarComp></NavBarComp>
     </div>
   );
 }
