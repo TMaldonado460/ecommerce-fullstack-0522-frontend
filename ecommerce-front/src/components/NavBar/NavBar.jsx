dcce02228c0b503789beb750adaa188529a7985d
Angeles Maza <angie.maza.89@gmail.com>
2022-05-07 17:11:37 -0300

    drop agregado links no funcionan

--- ecommerce-front/src/components/NavBar/NavBar.jsx
+++ ecommerce-front/src/components/NavBar/NavBar.jsx
@@ -1,7 +1,6 @@
 import "./NavBar.css";
 import {useState} from "react";
 import {FaAngleLeft, FaBars, FaCartArrowDown, FaUserCircle} from "react-icons/fa";
-
 export default function NavBar () {
     const [isDrop, setIsDrop] = useState(false);
     const handleClick = () => {
@@ -12,6 +11,13 @@ export default function NavBar () {
                 <div className="icon-drop" onClick={handleClick}>
                     {isDrop ? <FaAngleLeft />:<FaBars />}
                 </div>
+                 <ul className={isDrop ? 'nav-menu active': 'nav-menu'}>
+                    <li>Home</li>
+                    <li>Mujeres</li>
+                    <li>Hombres</li>
+                    <li>Insumos</li>
+                    <li>Contacto</li>
+                </ul> 
                 <div>
                     <h3>Proyecto Vacaciones</h3>
                 </div>
